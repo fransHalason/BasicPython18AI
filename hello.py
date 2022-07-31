3335ede8bca9147adca48139f58774c49e9841ba
fransHalason <franshalasonn@gmail.com>
2022-07-31 14:00:01 +0700

    ada perubahan dalam file ini

--- hello.py
+++ hello.py
@@ -1,2 +1,58 @@
-print("Hello World")
+'''
+ini adalah sebuah komentar satu
+ini adalah sebuah komentar dua
+'''
 
+# print("Hello World") # ini adalah sebuah komentar
+
+# x = 5
+# y = "Hello World"
+
+# print(x)
+# print("\n")
+# print(y)
+
+
+# x = 5
+
+# print(type(x))
+
+# print("======================")
+
+# y = 20.1
+
+# print(type(y))
+
+# print("======================")
+
+# z = "Hello World"
+# print(type(z))
+# print(type('Hello, World!'))
+
+# print("======================")
+
+# a = True
+# b = False
+# print(type(a))
+# print(type(b))
+
+
+# x = 4
+# y = 10
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x ** y)
+
+# print(y % x)
+
+x = int(2.8)
+y = float("4.2")
+z = str(3.0) 
+
+print(x)
+print(type(x))
+print(y)
+print(z)
